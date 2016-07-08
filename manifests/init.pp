34bcc3a077aec0f6c02f98fc9fda49732eb0a1bc
Pete Ezzo <peter.ezzo@gmail.com>
2016-07-08 13:06:01 -0400

    migration to new ssh module and jumphost role

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,24 +1,50 @@
-class ssh (
-    $ssh_package = "openssh-server",
-    $ssh_daemon  = "ssh",
-    $ports = [ '22' ],
-){
-    # lets install ssh
-    package { "$ssh_package":
-        ensure => "present",
-        before => File['/etc/ssh/sshd_config'],
-   }
+# This class installs latest ssh server (though it almost always is already)
+# Also triggers edits the config files per stanzas in hiera using augeas
+class ssh(
+  $sshd_config  = {},
+  $ssh_config   = {},
+  $install_mosh = true,
+) {
+  #require base  # doesn't actually require the base for anything
 
-   file { '/etc/ssh/sshd_config':
-        ensure  => file,
-        mode    => 600,
-        content => template('ssh/sshd_config.erb'),
-   }
+  # put this in a params class?  is that still a thing done with puppet?
+  #  $package     = $ssh::params::package,
+  #  $service     = $ssh::params::service,
+  # i only use RHEL-clones and Ubuntu LTS at the moment
+  case $::osfamily {
+    'Debian': {
+      $package = 'openssh-server'
+      $service = 'ssh'
+      $mosh    = 'mosh'
+    }
+    'RedHat': {
+      $package = 'openssh-server'
+      $service = 'sshd'
+      $mosh    = 'mosh'
+    }
+    default: {
+      fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
+    }
+  }
 
-    service { "$ssh_daemon":
-        ensure    => running,
-        enable    => true,
-        subscribe => File['/etc/ssh/sshd_config'],
-   }
+  package { $package:
+    ensure  => 'latest',
+  }
+  service { $service:
+    ensure  => 'running',
+    enable  => true,
+    require => Package[$package]
+  }
 
+  # these pull hash from hiera and dump it into augeas to edit config files
+  # augeas will handle restart of sshd if file is updated
+  create_resources(sshd_config, $sshd_config)
+  create_resources(ssh_config, $ssh_config)
+
+  # install mosh by default for more robust ssh
+  if install_mosh {
+    package { $mosh:
+      ensure => 'latest',
+    }
+  }
 }
